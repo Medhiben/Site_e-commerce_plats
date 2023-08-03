1e86942b5bd6337bde78953cba249fcb1fb7b741
Medhi Ramdani <m.ramdani@groupeonepoint.com>
2023-08-03 14:03:59 +0200

    changes commit

--- js/dish.js
+++ js/dish.js
@@ -1,9 +1,2 @@
- export class Dish {
-    constructor(name,description, price, category) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.category = category;
-    }
-}
+
 
