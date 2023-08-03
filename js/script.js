605e9e4de65ede6cd5364833bda2f4a5b79422f2
Medhi Ramdani <m.ramdani@groupeonepoint.com>
2023-08-03 17:35:22 +0200

    add html from js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- class Dish {
+class Dish {
     constructor(name,description, price, category) {
         this.name = name;
         this.description = description;
@@ -6,7 +6,6 @@
         this.category = category;
     }
 }
-
 //recuperer par un fetch les données du dossier plats.json
 const PLATS_URL ='./js/plats.json';
 const tableauDish = [];
@@ -22,13 +21,17 @@ for(let dish of data){
  const newDish = new Dish(dish.name,dish.description, dish.price, dish.category);
  tableauDish.push(newDish);
 }
+for(let dish of tableauDish){
+   displayPlat(dish);
+}
 console.log(tableauDish);
 
 })
 
 //creer les elements html en javascript , Récupérer l' id "menu-list" et insérer un noeud "li" qui représente un plat 
- function Plats(plats){
+ function displayPlat(plats){
 let ul = document.getElementById("menu-list");
+
 
 
 // créer une boucle for pour création et ajout d'element html en js
@@ -37,6 +40,7 @@ let ul = document.getElementById("menu-list");
 
 //insertion de l'élément li
 let productLi = document.createElement('li');
+productLi.classList.add('menu-item');
 ul.appendChild(productLi);
 
 
@@ -46,8 +50,8 @@ productLi.appendChild(divContenair)
 
 // creation de l'element img
 const image = document.createElement('img');
-img.src = 'chemin/vers/image2.jpg';
-img.alt = 'Image du plat 2';
+image.src = 'assets/image2.jpg';
+image.alt = 'Image du plat 2';
 divContenair.appendChild(image);
 
 
@@ -58,19 +62,19 @@ divContenair.appendChild(divMenuItem);
 
 //creation du h3
 const h3 = document.createElement('h3');
-h3.textContent = 'Nom du plat 2';
+h3.textContent = plats.name;
 divMenuItem.appendChild(h3)
 
 
 //creation du p
 const p = document.createElement('p');
-p.textContent = 'Description du plat';
-h3.appendChild(p);
+p.textContent = plats.description;
+divMenuItem.appendChild(p);
 
 //creation d'une div
 const divmenuItemprice = document.createElement('div');
 divmenuItemprice.classList.add("menu-item-price");
-divmenuItemprice.textContent = 'Prix du plat 2';
+divmenuItemprice.textContent = plats.price + '€';
 divContenair.appendChild(divmenuItemprice);
 
 //creation d'une div
@@ -80,30 +84,17 @@ productLi.appendChild(divButton);
 
 //creation de bouton avec une class et un texte 
 const buttonSuccess = document.createElement('button');
-buttonSuccess.classList.add("btn btn success");
+buttonSuccess.classList.add("btn");
+buttonSuccess.classList.add("btn-success");
 buttonSuccess.textContent = 'ajouter au panier';
 divButton.appendChild(buttonSuccess);
 
 //creation de bouton avec une class et un texte 
 const buttonDanger = document.createElement('button');
-buttonSuccess.classList.add("btn btn danger");
+buttonDanger.classList.add("btn");
+buttonDanger.classList.add("btn-danger");
 buttonDanger.textContent = 'Voir la description';
 divButton.appendChild(buttonDanger);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
